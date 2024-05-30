0399066e336f569e3e81fd40aa2be431c93012a7
Piotr Archi <piotr.borysowski@gmail.com>
2024-05-30 11:20:24 +0200

    add gdown

--- drive.py
+++ drive.py
@@ -1,15 +1,16 @@
 import os.path
-import io
+import gdown
 import sys
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
+GOOGLE_DRIVE=''
 
 def main():
   """Shows basic usage of the Drive v3 API.
@@ -38,69 +39,35 @@ def main():
     service = build("drive", "v3", credentials=creds)
 
     # Call the Drive v3 API
-    results = (
-        service.files()
-        .list(pageSize=10, supportsAllDrives=True, includeItemsFromAllDrives=True, q=f'name = \'{sys.argv[1]}\' and mimeType = \'application/vnd.google-apps.folder\'', fields="nextPageToken, files(id, name)")
+    results_all = (
+        service.files() #
+        .list(pageSize=10, supportsAllDrives=True, includeItemsFromAllDrives=True, q=f'\'{GOOGLE_DRIVE}\' in parents  and mimeType = \'application/vnd.google-apps.folder\'', fields="nextPageToken, files(id)")
         .execute()
     )
-    items = results.get("files", [])
 
+    items = results_all.get("files", [])
+    # print(items)
+  #
     if not items:
-      print("No files found.")
+      # print("No folders found.")
       return
     for item in items:
       folderId = item['id']
-      destinationFolder = sys.argv[1]
-      downloadFolder(service, folderId, destinationFolder)
+      results = (
+        service.files() #and name = \'{sys.argv[1]}\'
+        .list(pageSize=10, supportsAllDrives=True, includeItemsFromAllDrives=True, q=f'\'{folderId}\' in parents and name = \'{sys.argv[1]}\' and mimeType = \'application/vnd.google-apps.folder\'', fields="nextPageToken, files(id, name)")
+        .execute()
+      )
+      items_in_folder = results.get("files", [])
+      if not items_in_folder:
+          continue
+      for item_i in items_in_folder:
+          url = f"https://drive.google.com/drive/folders/{item_i['id']}"
+          gdown.download_folder(url)
+          return
   except HttpError as error:
     # TODO(developer) - Handle errors from drive API.
     print(f"An error occurred: {error}")
-
-
-def downloadFolder(service, fileId, destinationFolder):
-    if not os.path.isdir(destinationFolder):
-        os.mkdir(path=destinationFolder)
-
-    results = service.files().list(
-        pageSize=300,
-        q="parents in '{0}'".format(fileId),
-        fields="files(id, name, mimeType)"
-        ).execute()
-
-    items = results.get('files', [])
-
-    for item in items:
-        itemName = item['name']
-        itemId = item['id']
-        itemType = item['mimeType']
-        filePath = destinationFolder + "/" + itemName
-
-        if itemType == 'application/vnd.google-apps.folder':
-            print("Stepping into folder: {0}".format(filePath))
-            downloadFolder(service, itemId, filePath) # Recursive call
-        elif not itemType.startswith('application/'):
-            downloadFile(service, itemId, filePath)
-        else:
-            print("Unsupported file: {0}".format(itemName))
-
-
-def downloadFile(service, fileId, filePath):
-    # Note: The parent folders in filePath must exist
-    print("-> Downloading file with id: {0} name: {1}".format(fileId, filePath))
-    request = service.files().get_media(fileId=fileId)
-    fh = io.FileIO(filePath, mode='wb')
-
-    try:
-        downloader = MediaIoBaseDownload(fh, request, chunksize=1024*1024)
-
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk(num_retries = 2)
-            if status:
-                print("Download %d%%." % int(status.progress() * 100))
-        print("Download Complete!")
-    finally:
-        fh.close()
 
 if __name__ == "__main__":
     if(len(sys.argv) > 1):
